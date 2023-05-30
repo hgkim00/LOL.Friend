745b3ca970d7d63928a92d5c86d39d94b7062c10
Hyeongi Kim <21900215@handong.ac.kr>
2023-05-31 04:46:51 +0900

    FEAT[#4]: Nav 위젯 생성 및 각 페이지 파일 생성

--- lib/views/rank.dart
+++ lib/views/rank.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:lol_friend/services/auth_service.dart';
 import 'package:lol_friend/widgets/bottom_navigation.dart';
-import 'package:provider/provider.dart';
 
 class RankPage extends StatelessWidget {
   const RankPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    AuthService auth = Provider.of<AuthService>(context);
-
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
@@ -20,14 +16,6 @@ class RankPage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Image.asset('assets/images/appbar.png'),
-          actions: [
-            IconButton(
-              onPressed: () {
-                auth.logout();
-              },
-              icon: const Icon(Icons.logout),
-            ),
-          ],
         ),
         body: Column(
           children: const [
